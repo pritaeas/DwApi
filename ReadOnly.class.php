2ec0132d8dc135674b185ca2bfaa44ae084f8a65
pritaeas <pritaeas@hotmail.com>
2013-04-03 19:15:04 +0200

    Added method implementations.

--- ReadOnly.class.php
+++ ReadOnly.class.php
@@ -13,17 +13,37 @@ class ReadOnly extends Base
 
     public function GetMemberActivityPoints($id)
     {
+        if (!is_int($id) or ($id < 1))
+        {
+            return false;
+        }
 
+        return $this->GetUrl("http://www.daniweb.com/api/members/{$id}/activities");
     }
 
     public function GetMemberEndorsements($id)
     {
+        if (!is_int($id) or ($id < 1))
+        {
+            return false;
+        }
 
+        return $this->GetUrl("http://www.daniweb.com/api/members/{$id}/endorsements");
     }
 
     public function GetMemberReputationComments($id, $page)
     {
+        if (!is_int($id) or ($id < 1))
+        {
+            return false;
+        }
 
+        $url = "http://www.daniweb.com/api/members/{$id}/comments";
+        if (is_int($page) and ($page > 0))
+        {
+            $url .= '?page=' . $page;
+        }
+        return $this->GetUrl($url);
     }
 
     public function GetMembers($userIds, $page)
@@ -36,9 +56,34 @@ class ReadOnly extends Base
 
     }
 
-    public function Search($query, $page)
+    public function SearchArticles($query, $page)
     {
+        if (empty($query))
+        {
+            return false;
+        }
 
+        $url = 'http://www.daniweb.com/api/articles/search?query=' . urlencode($query);
+        if (is_int($page) and ($page > 0))
+        {
+            $url .= '&page=' . $page;
+        }
+        return $this->GetUrl($url);
+    }
+
+    public function SearchMembers($username, $page)
+    {
+        if (empty($username))
+        {
+            return false;
+        }
+
+        $url = 'http://www.daniweb.com/api/members/search?query=' . urlencode($username);
+        if (is_int($page) and ($page > 0))
+        {
+            $url .= '&page=' . $page;
+        }
+        return $this->GetUrl($url);
     }
 }
 ?>
