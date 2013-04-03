823282c0c548889577e524c8fa0dab9acf40bad8
pritaeas <pritaeas@hotmail.com>
2013-04-03 11:00:17 +0200

    Stub for open/unauthenticated API calls

---
+++ ReadOnly.class.php
@@ -0,0 +1,44 @@
+<?php
+class ReadOnly extends Base
+{
+    public function GetArticles($articleIds, $articleType, $orderBy, $page, $forumId)
+    {
+
+    }
+
+    public function GetForums($forumsIds, $includeSelf)
+    {
+
+    }
+
+    public function GetMemberActivityPoints($id)
+    {
+
+    }
+
+    public function GetMemberEndorsements($id)
+    {
+
+    }
+
+    public function GetMemberReputationComments($id, $page)
+    {
+
+    }
+
+    public function GetMembers($userIds, $page)
+    {
+
+    }
+
+    public function GetPosts($page, $postType)
+    {
+
+    }
+
+    public function Search($query, $page)
+    {
+
+    }
+}
+?>
