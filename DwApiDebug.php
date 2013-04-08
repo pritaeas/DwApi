9546c4279ccde06a9f1205216b55f7faae2cc67b
pritaeas <pritaeas@hotmail.com>
2013-04-08 23:02:43 +0200

    Added new tests.

--- DwApiDebug.php
+++ DwApiDebug.php
@@ -1,9 +1,11 @@
 <?php
-include '../Classes/DwApi/DwApiOAuth.class.php';
+include 'DwApiOAuth.class.php';
 
-$dwApi = new DwApiOAuth('');
+$dwApi = new DwApiOAuth('AccessToken');
+
 
 // DwApiBase
+
 $openArticleTypes = $dwApi->GetArticleTypes(true, true);
 $oauthArticleTypes = $dwApi->GetArticleTypes(false, true);
 
@@ -19,9 +21,11 @@ $result = $dwApi->GetRelationTypes(true);
 $result = $dwApi->GetRelationTypes(false);
 $result = $dwApi->GetRelationTypes(2);
 
+
 // DwApiRss
+
 $result = $dwApi->GetFeed();
-$result = $dwApi->GetFeed(1);
+$result = $dwApi->GetFeed(17);
 $result = $dwApi->GetFeed(-1);
 $result = $dwApi->GetFeed('1');
 
@@ -32,9 +36,32 @@ foreach ($openArticleTypes as $articleType)
 
 $result = $dwApi->GetFeed(1, 'invalid');
 
+
 // DwApiOpen
 
+$result = $dwApi->GetArticlePosts(451816);
+$result = $dwApi->GetArticles();
+$result = $dwApi->GetForumArticles(17);
+$result = $dwApi->GetForumPosts(17);
+$result = $dwApi->GetForums();
+$result = $dwApi->GetMemberActivityPoints(94719);
+$result = $dwApi->GetMemberArticles(94719);
+$result = $dwApi->GetMemberEndorsements(94719);
+$result = $dwApi->GetMemberPosts(94719);
+$result = $dwApi->GetMemberReputationComments(94719);
+$result = $dwApi->GetMembers();
+$result = $dwApi->GetPostReputationComments(1957463);
+$result = $dwApi->GetPosts();
+$result = $dwApi->SearchArticles('daniweb-api');
+$result = $dwApi->SearchMembers('pritaeas');
+
+
 // DwApiOAuth
+
+$result = $dwApi->GetArticles();
+$result = $dwApi->GetForumArticles(17);
+$result = $dwApi->GetMemberArticles(94719);
+
 $result = $dwApi->GetPrivateMessages(true);
 $result = $dwApi->GetPrivateMessages(false);
 $result = $dwApi->GetPrivateMessages('1');
